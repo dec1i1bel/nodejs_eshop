5a90c6140c2dc2a07371b4fc5cfc6a22c427912d
Vasiliy <v.balabanov@ddemo.ru>
2022-01-14 11:22:06 +0300

    .env.example, db connection, User model

--- server/index.js
+++ server/index.js
@@ -1,7 +1,23 @@
 require('dotenv').config();
 const express = require('express');
-const PORT = process.env.PORT || 5000;
+const sequelize = require('./db');
+const PORT = process.env.PORT || 5432;
 
 const app = express();
 
-app.listen(PORT, () => console.log('server started at port ' + PORT));
+// все операции с БД являются асинхронными
+const start = async() => {
+    try {
+        // подключение к БД
+        await sequelize.authenticate();
+
+        // сверяет состояние БД со схемой данных
+        await sequelize.sync();
+
+        app.listen(PORT, () => console.log('server started at port ' + PORT));
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
---
+++ server/models/models.js
@@ -0,0 +1,24 @@
+const sequelize = require('../db');
+
+// импорт класса DataTypes из sequelize.
+// нужен для описания типов данных
+const {DataTypes} = require('sequelize');
+
+const User = sequelize.define('user', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING
+    },
+    role: {
+        type: DataTypes.STRING,
+        defaultValue: 'USER'
+    }
+});
