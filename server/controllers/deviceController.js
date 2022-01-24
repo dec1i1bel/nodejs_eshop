7e0f6dfefc1a3be729299cfd6825c148ce94e6f4
Vasiliy <v.balabanov@ddemo.ru>
2022-01-24 14:49:54 +0300

    .

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -6,14 +6,14 @@ const ApiError = require('../error/ApiError');
 class DeviceController {
     async create(req, res, next) {
         try {
-            const {name, price, brand_id, type_id, info} = req.body;
+            const {name, price, brandId, typeId, info} = req.body;
             const {img} = req.files;
 
             let filename = uuid.v4() + '.jpg';
             
             img.mv(path.resolve(__dirname, '..', 'static', filename));
     
-            const device = await Device.create({name, price, brand_id, type_id, img: filename});
+            const device = await Device.create({name, price, brandId, typeId, img: filename});
 
             return res.json(device);
         } catch (err) {
@@ -22,8 +22,43 @@ class DeviceController {
 
     }
     
-    async getAll(req, res) {
+    async getAll(req, res, next) {
+        try {
+            // получаем из запроса id бренда и/или типа b данные для постраничной навигации
+            const {brandId, typeId, limit, page} = req.query;
 
+            const paginPage = page || 1;
+            const itemsLimit = limit || 6;
+
+            // отступ в запросе от начала списка товаров для данной страницы навигации
+            const offset = paginPage * itemsLimit - itemsLimit;
+    
+            let devices;
+            
+            // если в запросе нет ни бренда, ни типа
+            if ((typeof brandId === 'undefined') && (typeof typeId === 'undefined')) {
+                devices = await Device.findAll({limit: itemsLimit});
+            }
+    
+            // если только id бренда:
+            if ((typeof brandId !== 'undefined') && (typeof typeId === 'undefined')) {
+                devices = await Device.findAll({where:{brandId}, itemsLimit, offset});
+            }
+    
+            // если только id типа:
+            if ((typeof brandId === 'undefined') && (typeof typeId !== 'undefined')) {
+                devices = await Device.findAll({where:{typeId}, itemsLimit, offset});
+            }
+    
+            // если id типа и бренда:
+            if ((typeof brandId === 'undefined') && (typeof typeId !== 'undefined')) {
+                devices = await Device.findAll({where:{typeId, brandId}, itemsLimit, offset});
+            }
+    
+            return res.json(devices);
+        } catch (err) {
+            next(ApiError.badRequest(err.message));
+        }
     }
 
     async getOne(req, res) {
